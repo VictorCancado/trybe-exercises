3ab3c3319165fc313f98f56a7c2bbb8e678da964
Víctor Cançado <victor.cancado@gmail.com>
2020-11-02 17:25:24 -0300

    Atualiza exercício 5

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_5.js
@@ -0,0 +1,11 @@
+let anguloA = 90;
+let anguloB = 50;
+let anguloC = 40;
+
+let somaAngulos = anguloA + anguloB + anguloC;
+
+if (somaAngulos === 180) {
+    console.log(true);
+} else {
+    console.log(false);
+}
