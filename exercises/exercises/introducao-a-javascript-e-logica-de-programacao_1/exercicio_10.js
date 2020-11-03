3f646e367e6f5ee117592ee8c215172b0add863f
Víctor Cançado <victor.cancado@gmail.com>
2020-11-03 03:58:06 -0300

    Adiciona exercício 10

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_10.js
@@ -0,0 +1,14 @@
+let cpv = 500;
+let valorVenda = 1000;
+let imposto = cpv * 0.2;
+let valorCustoTotal = cpv + imposto;
+let lucroUnitario = valorVenda - valorCustoTotal;
+let quantidadeVendida = 1000;
+let lucroTotal = lucroUnitario * quantidadeVendida;
+
+
+switch (quantidadeVendida) {
+    case (> 0)
+}
+
+
