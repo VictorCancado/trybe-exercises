235e484de9f906ed8bdcc8626935ac5193bf0013
Víctor Cançado <victor.cancado@gmail.com>
2020-11-04 01:30:57 -0300

    Atualiza exercício 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_5.js
@@ -1,2 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*Utilizando for, descubra qual o maior valor contido no array e imprima-o;*/
 
+let biggestNumber = 0;
+
+for(var index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > biggestNumber) {
+        biggestNumber = numbers[index];
+    }
+}
+console.log(biggestNumber);
