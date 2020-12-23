4c05330ba01472e5c2248cce9f9bc099f9859d77
Víctor Cançado <victor.cancado@gmail.com>
2020-12-23 03:30:03 -0300

    Adiciona exercício 3

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2.js
@@ -2,4 +2,8 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
+oddsAndEvens.sort(function(a, b) {
+    return a - b;
+});
+
 console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
